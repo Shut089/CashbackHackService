aa6c38017fed27eb879d00c20c5b64fa1d2feeec
Anton Borodin <bor183@mail.ru>
2025-11-25 21:54:04 +0500

    Bug fixes for Issues

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -46,11 +46,11 @@ public class CashbackHackServiceTest {
     public void shouldCalculateRemainIfAmountIs0() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
-        int expected = 1000;
+        int expected = 0;
 
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
 
     }
 }
